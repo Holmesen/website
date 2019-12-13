a0601f3bdb46656e3d1c1aa7e49a127b7febeefc
linss <1315654996@qq.com>
2019-12-13 18:08:37 +0800

    完善相册上传页面

--- src/views/album/upload.vue
+++ src/views/album/upload.vue
@@ -3,21 +3,21 @@
     <div class="left">
       <span id="add-article" @click="addAlbum"><i class="el-icon-circle-plus" style="margin: auto 10px auto 0px;color:#f2f2f2;"></i>新建相册</span>
       <span id="article-list">
-        <span v-for="(item,index) in albumList" :key="index" @click="chooseAlbum(index)" :class="{'activeArt': activeIdx === index}"><span :title="item.name||'无标题'">{{item?(item.name || '无标题'):'无标题'}}</span><i class="el-icon-circle-close" style="margin: auto 7px auto auto;color:#f2f2f2;" v-if="albumList.length>1" @click.stop="removeAlbum(index)"></i></span>
+        <span v-for="(item,index) in albumList" :key="index" @click="chooseAlbum(index)" :class="{'activeArt': activeIdx === index}"><span :title="item.name||'无标题'">{{item?(item.name || '无标题'):'无标题'}}</span><i class="el-icon-circle-close" style="margin: auto 7px auto auto;color:#f2f2f2;" v-if="false && albumList.length>1" @click.stop="removeAlbum(index)"></i></span>
       </span>
     </div>
     <div class="right">
       <div class="album-info" :class="{'has-fold':isFold}">
         <el-row v-show="!isFold">
           <el-col :span="8"><span>相&nbsp;册&nbsp;名：</span><el-input v-model="active.name" :disabled="!isEdit"></el-input></el-col>
-          <el-col :span="8"><span>创&nbsp;建&nbsp;者：</span><el-input v-model="active.user" :disabled="true"></el-input></el-col>
+          <el-col :span="8"><span>创&nbsp;建&nbsp;者：</span><el-input v-model="user" :disabled="true"></el-input></el-col>
           <el-col :span="8"><span>创建时间：</span><el-input v-model="active.date" :disabled="true"></el-input></el-col>
         </el-row>
         <el-row v-show="!isFold">
           <!-- <el-col :span="8"><span>主&emsp;&emsp;题：</span><el-input></el-input></el-col> -->
           <el-col :span="8"><span>权&emsp;&emsp;限：</span><el-switch style="height: 40px;" :disabled="!isEdit" v-model="active.isPublic" inactive-text="私密" active-text="公开"></el-switch></el-col>
           <el-col :span="16"><span style="line-height:32px;">标&emsp;&emsp;签：</span>
-            <el-tag effect="dark" style="line-height:32px; height:32px;" :key="tag" v-for="tag in active.tags" :closable="isEdit" :disable-transitions="false" @close="handleClose(tag)">{{tag}}</el-tag>
+            <el-tag style="line-height:32px; height:32px;" :key="tag" v-for="tag in active.tags" :closable="isEdit" :disable-transitions="false" @close="handleClose(tag)">{{tag}}</el-tag>
             <el-input class="input-new-tag" style="width:100px;line-height:32px; height:32px;" v-if="inputVisible && isEdit" v-model="inputValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm"></el-input>
             <el-button v-else class="button-new-tag" style="line-height:32px; height:32px;" size="small" @click="showInput" v-show="isEdit">+ New Tag</el-button>
           </el-col>
@@ -25,19 +25,20 @@
         <el-row v-show="!isFold">
           <el-col :span="24"><span>相册描述：</span><el-input style="margin-right:20px;" v-model="active.description" :disabled="!isEdit" type="textarea" :rows="2" maxlength="100" show-word-limit></el-input></el-col>
         </el-row>
-        <el-row v-show="!isFold">
+        <el-row >
           <el-col :span="24">
-            <el-button round style="margin-left:auto;" type="warning" @click="edit(false)">{{isEdit?"取消修改":"修改信息"}}</el-button>
-            <el-button round style="margin-right:20px;" type="primary" @click="submitUpload">上传图片</el-button>
-            <el-button round style="margin-right:20px;" type="success" v-show="isEdit" @click="edit(true)">确认修改</el-button>
-            <el-button round style="margin-right:20px;" type="primary" v-show="!isEdit" @click="release">上传相册</el-button>
+            <el-button round style="margin:auto 10px auto auto;" icon="el-icon-edit" type="warning" @click="edit(false)">{{isEdit?"取消修改":"修改信息"}}</el-button>
+            <!-- <el-button round style="margin:auto 10px;" type="primary" @click="submitUpload">上传图片</el-button> -->
+            <el-button round style="margin:auto 10px;" type="primary" icon="el-icon-upload2" v-show="!isEdit" @click="release">更新相册</el-button>
+            <el-button round style="margin:auto 20px auto 10px;" type="success" icon="el-icon-document-checked" v-show="isEdit" @click="edit(true)">确认修改</el-button>
+            <el-button round style="margin:auto 20px auto 10px;" type="danger" icon="el-icon-delete" v-show="!isEdit" @click="deleteAlbum">删除相册</el-button>
           </el-col>
         </el-row>
-        <span v-show="isFold" style="font-size:15px; color:#cccccc; text-align:center;">查看更多相册信息</span>
+        <span v-show="isFold" style="font-size:15px; color:#cccccc; text-align:center; width:fit-content; position:absolute; left:calc(50% - 60px);">查看更多相册信息</span>
         <div class="fold" @click="isFold=!isFold"><i v-show="!isFold" class="el-icon-caret-top"></i><i v-show="isFold" class="el-icon-caret-bottom"></i></div>
       </div>
       <div class="picture" :class="{'has-fold2':isFold}">
-        <el-upload ref="upload" :file-list="active.photos" :auto-upload="false" multiple :action='"http://localhost:3000/upload/album-image?keyid="+ukeyid' list-type="picture-card" :on-success="pictureUploadSuccess" :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
+        <el-upload ref="upload" :file-list="active.photos" :auto-upload="true" multiple :action='"http://localhost:3000/upload/album-image?keyid="+ukeyid' list-type="picture-card" :before-remove="beforeRemove" :on-success="pictureUploadSuccess" :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
           <i class="el-icon-plus"></i>
         </el-upload>
         <el-dialog :visible.sync="dialogVisible">
@@ -50,18 +51,18 @@
 
 <script>
 import {releaseAlbum, updateAlbum, getAlbumList} from '../../apis/album.js'
-import {NowTime} from '../../utils/time'
+import {NowTime, UTC2Local} from '../../utils/time'
 export default {
   name: 'name',
   data() {
     return {
       ukeyid: this.$store.getters.keyid,
+      user: this.$store.getters.name,
       dialogImageUrl: '',
       dialogVisible: false,
       albumList: [
         {
           name: '', // 相册名
-          user: this.$store.getters.name, // 创建者
           date: '', // 创建日期
           tags: [], // 标签
           isPublic: false, // 是否公开
@@ -71,7 +72,6 @@ export default {
       ],
       active: {
         name: '', // 相册名
-        user: this.$store.getters.name, // 创建者
         date: '', // 创建日期
         tags: [], // 标签
         isPublic: false, // 是否公开
@@ -99,18 +99,22 @@ export default {
     pictureUploadSuccess(response, file, fileList) {
       this.active.photos = fileList
     },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${ file.name }？`);
+    },
     submitUpload() {
       this.$refs.upload.submit()
     },
+    // 新建相册
     addAlbum() {
       this.albumList.push({
         name: '',
-        user: this.$store.getters.name,
         date: '',
         tags: [],
         isPublic: false,
         description: '',
-        photos: []
+        photos: [],
+        isNew: true
       })
       this.activeIdx = this.albumList.length-1
       this.active = this.albumList[this.albumList.length-1]
@@ -125,6 +129,24 @@ export default {
       this.activeIdx = 0
       this.active = this.albumList[0]
       this.albumList.splice(index, 1)
+    },
+    // 删除相册
+    deleteAlbum() {
+      this.$confirm('将永久删除该相册, 是否继续?', '警告', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
     },
     handleClose(tag) {
       this.active.tags.splice(this.active.tags.indexOf(tag), 1)
@@ -154,65 +176,111 @@ export default {
       this.isEdit = !this.isEdit
     },
     release() {
+      if(!this.active.name) { this.$message.error("请填写相册名！"); return }
       this.openFullScreen()
       let list = []
       this.active.photos.forEach(el => {
         list.push(JSON.stringify({name: (el.response?el.response.data.name:el.name), url: (el.response?el.response.data.url:el.url)}))
       })
       let active = JSON.parse(JSON.stringify(this.active))
-      updateAlbum({
-        keyid: this.active.keyid,
-        name: this.active.name,
-        isPublic: this.active.isPublic?'Y':'N',
-        photos: list,
-        tags: this.active.tags
-      }).then(res=> {
-        if(res.data.success) {
+      if(this.active.isNew) {
+        //上传新相册
+        releaseAlbum(
+          Object.assign(active, {ukeyid: this.$store.getters.keyid || '', isPublic:this.active.isPublic?'Y':'N', photos: list})
+        ).then(res=> {
+          if(res.data.success) {
+            this.$notify({
+              title: '新增相册',
+              message: res.data.message || '相册新增成功',
+              type: 'success'
+            })
+            this.active.isNew = false
+            this.getAlbumList()
+          } else {
+            this.$notify({
+              title: '新增相册',
+              message: res.data.message || '相册新增失败',
+              type: 'error'
+            })
+          }
+        }).catch(err=> {
+          this.$notify({
+            title: '新增相册',
+            message: '相册新增失败',
+            type: 'error'
+          })
+          console.error(err)
+        }).finally(()=> { this.loading.close() })
+      } else {
+        // 更新相册
+        updateAlbum({
+          keyid: this.active.keyid,
+          name: this.active.name,
+          isPublic: this.active.isPublic?'Y':'N',
+          photos: list,
+          tags: this.active.tags
+        }).then(res=> {
+          if(res.data.success) {
+            this.$notify({
+              title: '更新相册',
+              message: res.data.message || '相册更新成功',
+              type: 'success'
+            })
+            // this.active = res.data.data
+            this.getAlbumList()
+          } else {
+            this.$notify({
+              title: '更新相册',
+              message: res.data.message || '相册更新失败',
+              type: 'error'
+            })
+          }
+        }).catch(err=> {
           this.$notify({
             title: '更新相册',
-            message: res.data.message || '相册更新成功',
-            type: 'success'
+            message: '相册更新失败',
+            type: 'error'
           })
-          // this.active = res.data.data
+          console.error(err)
+        }).finally(()=> { this.loading.close() })
+      }
+    },
+    // 获取相册列表
+    getAlbumList() {
+      getAlbumList({}).then(res=> {
+        if(res.data.success) {
+          if(res.data.data && res.data.data.length>0) {
+            res.data.data.forEach((el,idx) => {
+              res.data.data[idx]["isPublic"] = (el.isPublic==='Y'?true:false)
+              res.data.data[idx]["date"] = UTC2Local(res.data.data[idx]["date"])
+            })
+            this.albumList = res.data.data
+            this.active = (!!this.albumList && this.albumList.length>0) ? this.albumList[0] : {name: this.$store.getters.name, user: '', date: '', tags: [], isPublic: false, description: '', photos: []}
+          }
         } else {
           this.$notify({
-            title: '更新相册',
-            message: res.data.message || '相册更新失败',
+            title: '获取相册',
+            message: res.data.message || '相册获取失败',
             type: 'error'
           })
         }
       }).catch(err=> {
+        console.error(err)
         this.$notify({
-          title: '更新相册',
-          message: '相册更新失败',
+          title: '获取相册',
+          message: '相册获取失败',
           type: 'error'
         })
-        console.error(err)
-      }).finally(()=> { this.loading.close() })
-      // releaseAlbum(
-      //   Object.assign(active, {ukeyid: this.$store.getters.keyid || '', isPublic:this.active.isPublic?'Y':'N', photos: list})
-      // ).then(res=> {
-      //   if(res.data.success) {
-      //     this.$notify({
-      //       title: '上传相册',
-      //       message: res.data.message || '相册上传成功',
-      //       type: 'success'
-      //     })
-      //   } else {
-      //     this.$notify({
-      //       title: '上传相册',
-      //       message: res.data.message || '相册上传失败',
-      //       type: 'error'
-      //     })
-      //   }
-      // }).catch(err=> {
-      //   this.$notify({
-      //     title: '上传相册',
-      //     message: '相册上传失败',
-      //     type: 'error'
-      //   })
-      //   console.error(err)
-      // }).finally(()=> { this.loading.close() })
+      }).finally(()=>{
+        this.active = (!!this.albumList && this.albumList.length>0) ? this.albumList[0] : {name: this.$store.getters.name, user: '', date: '', tags: [], isPublic: false, description: '', photos: []}
+        if(!this.active.date) {
+          debugger
+          setInterval(() => {
+            this.active.date = this.dateTime = NowTime()
+          }, 500)
+        }
+        this.loading.close()
+      })
     },
     openFullScreen() {
       this.loading = this.$loading({
@@ -225,36 +293,7 @@ export default {
   },
   mounted () {
     this.openFullScreen()
-    getAlbumList({}).then(res=> {
-      if(res.data.success) {
-        if(res.data.data && res.data.data.length>0) {
-          res.data.data.forEach((el,idx) => {
-            res.data.data[idx]["isPublic"] = (el.isPublic==='Y'?true:false)
-          })
-          this.albumList = res.data.data
-          this.active = !!this.albumList ? this.albumList[0] : {name: this.$store.getters.name, user: '', date: '', tags: [], isPublic: false, description: '', photos: []}
-        }
-      } else {
-        this.$notify({
-          title: '获取相册',
-          message: res.data.message || '相册获取失败',
-          type: 'error'
-        })
-      }
-    }).catch(err=> {
-      console.error(err)
-      this.$notify({
-        title: '获取相册',
-        message: '相册获取失败',
-        type: 'error'
-      })
-    }).finally(()=>{this.loading.close()})
-    this.active = !!this.albumList ? this.albumList[0] : {name: this.$store.getters.name, user: '', date: '', tags: [], isPublic: false, description: '', photos: []}
-    if(!this.active.date) {
-      setInterval(() => {
-        this.active.date = this.dateTime = NowTime()
-      }, 500)
-    }
+    this.getAlbumList()
   },
   watch: {
     // 'tempList': {
@@ -332,7 +371,7 @@ export default {
   border-top-left-radius: 7px; border-top-right-radius: 7px; font-size: 20px; cursor: pointer;
 }
 .has-fold{
-  height: 20px; min-height: 20px;
+  height: 45px; min-height: 45px;
 }
 .has-fold2{
   height: calc(100% - 20px); max-height: calc(100% - 20px);
